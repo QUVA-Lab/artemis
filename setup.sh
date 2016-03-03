fa4816b124875125f3047df447a7f0289b2e6c56
Peter O'Connor <peter.ed.oconnor@gmail.com>
2016-03-03 11:36:02 +0100

    ok

--- setup.sh
+++ setup.sh
@@ -1,6 +1,5 @@
 virtualenv venv
-source setup_git_filters.sh
 printf '\nexport PYTHONPATH=$PYTHONPATH:%s\n' $PWD >> venv/bin/activate
 source venv/bin/activate
 pip install -r requirements.txt
-echo "Plato Setup Complete! (As long as there're no errors above)"
+echo "Setup Complete! (As long as there're no errors above)"
