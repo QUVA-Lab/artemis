4ae5a367e3efe7ac38ed24e802f614ba8abf7f8f
Peter O'Connor <peter.ed.oconnor@gmail.com>
2016-04-29 12:35:09 +0200

    added setup file for pip installation

---
+++ setup.py
@@ -0,0 +1,17 @@
+
+
+from setuptools import setup
+import plato
+import utils
+import plotting
+import general
+
+
+setup(name='artemis',
+      author='Peter & Matthias',
+      author_email='poconn4@gmail.com',
+      url='https://github.com/quva-lab/artemis',
+      long_description='A Library for plotting and managing experiments.',
+      version=0,
+      packages=['artemis'],
+      scripts=[])
