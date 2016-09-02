b6a211d9933ce05c6c779ee161ab72b59f6cc371
Peter O'Connor <peter.ed.oconnor@gmail.com>
2016-09-02 14:29:55 +0200

    can now download archives

--- artemis/fileman/file_getter.py
+++ artemis/fileman/file_getter.py
@@ -3,8 +3,10 @@ import tempfile
 import urllib2
 from StringIO import StringIO
 import gzip
-
+import tarfile
+from zipfile import ZipFile
 from artemis.fileman.local_dir import get_local_path
+from artemis.general.should_be_builtins import bad_value
 import os
 
 __author__ = 'peter'
@@ -36,6 +38,77 @@ def get_file(relative_name, url = None, data_transformation = None):
         with open(full_filename, 'w') as f:
             f.write(data)
     return full_filename
+
+
+def get_file_in_zip(relative_path, subpath, url, force_extract = False):
+    """
+    Download a zip file, unpack it, and get the local address of a file within this zip (so that you can open it, etc).
+
+    :param relative_path: Local name for the extracted folder.  (Zip file will be named this with the appropriate zip extension)
+    :param url: Url of the zip file to download
+    :param subpath: Path of the file relative to the zip folder.
+    :param force_extract: Force the zip file to re-extract (rather than just reusing the extracted folder)
+    :return: The full path to the file on your system.
+    """
+    local_folder_path = get_archive(relative_path=relative_path, url=url, force_extract=force_extract)
+    local_file_path = os.path.join(local_folder_path, subpath)
+    assert os.path.exists(local_file_path), 'Could not find the file "%s" within the extracted folder: "%s"' % (subpath, local_folder_path)
+    return local_file_path
+
+
+def get_archive(relative_path, url, force_extract=False, archive_type = None):
+    """
+    Download a compressed archive and extract it into a folder.
+
+    :param relative_path: Local name for the extracted folder.  (Zip file will be named this with the appropriate zip extension)
+    :param url: Url of the archive to download
+    :param force_extract: Force the zip file to re-extract (rather than just reusing the extracted folder)
+    :return: The full path to the extracted folder on your system.
+    """
+
+    local_folder_path = get_local_path(relative_path)
+
+    assert archive_type in ('.tar.gz', '.zip', None)
+
+    if not os.path.exists(local_folder_path):  # If the folder does not exist, download zip and extract
+        response = urllib2.urlopen(url)
+
+        # Need to infer
+        if archive_type is None:
+            if url.endswith('.tar.gz'):
+                archive_type = '.tar.gz'
+            elif url.endswith('.zip'):
+                archive_type = '.zip'
+            else:
+                info = response.info()
+                try:
+                    header = next(x for x in info.headers if x.startswith('Content-Disposition'))
+                    original_file_name = next(x for x in header.split(';') if x.startswith('filename')).split('=')[-1].lstrip('"\'').rstrip('"\'')
+                    archive_type = '.tar.gz' if original_file_name.endswith('.tar.gz') else '.zip' if original_file_name.endswith('.zip') else \
+                        bad_value(original_file_name, 'Filename "%s" does not end with a familiar zip extension like .zip or .tar.gz' % (original_file_name, ))
+                except StopIteration:
+                    raise Exception("Could not infer archive type from user argument, url-name, or file-header.  Please specify archive type as either '.zip' or '.tar.gz'.")
+        print 'Downloading archive from url: "%s"...' % (url, )
+        data = response.read()
+        print '...Done.'
+
+        local_zip_path = local_folder_path + archive_type
+        with open(local_zip_path, 'w') as f:
+            f.write(data)
+
+        force_extract = True
+
+    if force_extract:
+        if archive_type == '.tar.gz':
+            with tarfile.open(local_zip_path) as f:
+                f.extractall(local_folder_path)
+        elif archive_type == '.zip':
+            with ZipFile(local_zip_path) as f:
+                f.extractall(local_folder_path)
+        else:
+            raise Exception()
+
+    return local_folder_path
 
 
 def get_file_and_cache(url, data_transformation = None, enable_cache_write = True, enable_cache_read = True):
