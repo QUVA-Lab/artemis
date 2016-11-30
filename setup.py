b276ce18e3d8807a8cfccce707ce247f6bc7a52e
Peter O'Connor <peter.ed.oconnor@gmail.com>
2016-11-30 14:04:01 +0100

    recursively find packages

--- setup.py
+++ setup.py
@@ -1,15 +1,11 @@
+from setuptools import setup, find_packages
 
-
-from setuptools import setup
-import os
-
-CONFIG_PATH = os.path.join(os.path.expanduser('~'), '.artemisrc')
-
-setup(name='artemis-ml',
-      author='Peter & Matthias',
-      author_email='poconn4@gmail.com',
-      url='https://github.com/quva-lab/artemis',
-      long_description='A Library for plotting and managing experiments.',
-      version=0,
-      packages=['artemis-ml'],
-      scripts=[])
+setup(
+    name='artemis-ml',
+    author='Peter & Matthias',
+    author_email='poconn4@gmail.com',
+    url='https://github.com/quva-lab/artemis',
+    long_description='A Library for plotting and managing experiments.',
+    version=1,
+    packages=find_packages(),
+    scripts=[])
