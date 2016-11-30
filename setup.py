10a223b9749310596edcafa148aa3bd4e7582245
Peter O'Connor <peter.ed.oconnor@gmail.com>
2016-11-30 13:46:33 +0100

    changed package name

--- setup.py
+++ setup.py
@@ -5,11 +5,11 @@ import os
 
 CONFIG_PATH = os.path.join(os.path.expanduser('~'), '.artemisrc')
 
-setup(name='artemis',
+setup(name='artemis-ml',
       author='Peter & Matthias',
       author_email='poconn4@gmail.com',
       url='https://github.com/quva-lab/artemis',
       long_description='A Library for plotting and managing experiments.',
       version=0,
-      packages=['artemis'],
+      packages=['artemis-ml'],
       scripts=[])
