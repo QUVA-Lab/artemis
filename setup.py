db138c14d78b9a3100a068c79087cdc46b5445b8
Peter O'Connor <peter.ed.oconnor@gmail.com>
2016-11-30 14:13:30 +0100

    description

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     author='Peter & Matthias',
     author_email='poconn4@gmail.com',
     url='https://github.com/quva-lab/artemis',
-    long_description='A Library for plotting and managing experiments.',
+    long_description='Artemis aims to get rid of all the boring, bureaucratic coding (plotting, file management, etc) involved in machine learning projects, so you can get to the good stuff quickly.',
     install_requires=['numpy', 'scipy', 'matplotlib', 'pytest', 'pillow', 'tabulate'],
     version=1,
     packages=find_packages(),
