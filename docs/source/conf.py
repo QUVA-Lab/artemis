81fde3f21dc950055dacc44531a13fe2ff98a94d
Peter O'Connor <peter.ed.oconnor@gmail.com>
2017-08-03 14:49:59 +0200

    ok, try setting matplotlib backend in conf

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -20,6 +20,8 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('../..'))
 
+import matplotlib
+matplotlib.use('agg')
 
 # -- General configuration ------------------------------------------------
 
