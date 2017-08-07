34b93fb2322ce16250899217d3ae3a59b4dbf2da
Peter O'Connor <peter.ed.oconnor@gmail.com>
2017-08-07 13:23:39 +0200

    docstring

--- artemis/general/hashing.py
+++ artemis/general/hashing.py
@@ -21,39 +21,42 @@ def fixed_hash_eq(obj1, obj2):
     return compute_fixed_hash(obj1)==compute_fixed_hash(obj2)
 
 
-def compute_fixed_hash(obj, hasher = None, try_objects=False, memo = None, count=None):
+def compute_fixed_hash(obj, try_objects=False, _hasher = None, _memo = None, _count=None):
     """
     Given an object, return a hash that will always be the same (not just for the lifetime of the
     object, but for all future runs of the program too).
     :param obj: Some nested container of primitives
-    :param hasher: (for internal use - note that this is stateful, so calling this function with this argument changes
+    :param try_objects: Try to break into objects
+    :param _hasher: (for internal use - note that this is stateful, so calling this function with this argument changes
         the hasher object)
+    :param _memo: (for internal use - to remember hashed objects and avoid infinite recursion)
+    :param _count: (for internal use - to uniquely identify objects with circular references)
     :return: A 32-character hexidecimal (0-f) hash code for your object.
     """
-    if memo is None:
-        memo = {}
-    elif not isinstance(obj, (np.ndarray, int, float, basestring, bool)) and id(obj) in memo:
-        hasher.update(_ALREADY_SEEN_CODE)
-        hasher.update(str(memo[id(obj)]))
-        hasher.update(_END_CODE)
-        return memo[id(obj)]
+    if _memo is None:
+        _memo = {}
+    elif not isinstance(obj, (np.ndarray, int, float, basestring, bool)) and id(obj) in _memo:
+        _hasher.update(_ALREADY_SEEN_CODE)
+        _hasher.update(str(_memo[id(obj)]))
+        _hasher.update(_END_CODE)
+        return _memo[id(obj)]
 
-    if count is None:
-        count = itertools.count()
-    memo[id(obj)] = count.next()
+    if _count is None:
+        _count = itertools.count()
+    _memo[id(obj)] = _count.next()
 
-    if hasher is None:
-        hasher = hashlib.md5()
+    if _hasher is None:
+        _hasher = hashlib.md5()
 
-    kwargs = dict(hasher=hasher, try_objects=try_objects, memo=memo, count=count)
+    kwargs = dict(hasher=_hasher, try_objects=try_objects, memo=_memo, count=_count)
 
-    hasher.update(obj.__class__.__name__)
+    _hasher.update(obj.__class__.__name__)
     if isinstance(obj, np.ndarray):
-        hasher.update(pickle.dumps(obj.dtype, protocol=2))
-        hasher.update(pickle.dumps(obj.shape, protocol=2))
-        hasher.update(obj.tostring())
+        _hasher.update(pickle.dumps(obj.dtype, protocol=2))
+        _hasher.update(pickle.dumps(obj.shape, protocol=2))
+        _hasher.update(obj.tostring())
     elif isinstance(obj, (int, basestring, float, bool)) or (obj is None) or (obj in (int, str, float, bool)):
-        hasher.update(pickle.dumps(obj, protocol=2))
+        _hasher.update(pickle.dumps(obj, protocol=2))
     elif isinstance(obj, (list, tuple)):
         for el in obj:
             compute_fixed_hash(el, **kwargs)
@@ -78,9 +81,9 @@ def compute_fixed_hash(obj, hasher = None, try_objects=False, memo = None, count
         # TODO: Consider whether to pickle by default.  Note that pickle strings are not necessairly the same for identical objects.
         raise NotImplementedError("Don't have a method for hashing this %s" % (obj, ))
 
-    hasher.update(_END_CODE)  # Necessary to distinguish ([a, b], c) from ([a, b, c])
-    result = hasher.hexdigest()
-    memo[id(obj)] = result
+    _hasher.update(_END_CODE)  # Necessary to distinguish ([a, b], c) from ([a, b, c])
+    result = _hasher.hexdigest()
+    _memo[id(obj)] = result
     return result
 
 
