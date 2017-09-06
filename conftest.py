4bceee34d6986e52dfb03f46c8e35d70e7142da1
Peter O'Connor <peter.ed.oconnor@gmail.com>
2017-09-06 08:56:52 +0200

    try agg thing in conftest.py

--- conftest.py
+++ conftest.py
@@ -4,6 +4,9 @@ import sys
 def pytest_configure(config):
     import sys
     sys._called_from_test = True
+    import matplotlib
+    matplotlib.use('Agg')
+
 
 def pytest_unconfigure(config):
     if hasattr(sys, '_called_from_test'):
