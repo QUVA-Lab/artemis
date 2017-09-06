37cc63c9b3255a29fb61213746d140090cc28e5e
Peter O'Connor <peter.ed.oconnor@gmail.com>
2017-09-06 09:59:28 +0200

    well that wasnt so hard

--- conftest.py
+++ conftest.py
@@ -1,11 +1,12 @@
 import sys
+import os
 # content of conftest.py
 
 def pytest_configure(config):
-    import sys
     sys._called_from_test = True
-    import matplotlib
-    matplotlib.use('Agg')
+    if os.environ.get('TRAVIS'):
+        import matplotlib
+        matplotlib.use('Agg')
 
 
 def pytest_unconfigure(config):
