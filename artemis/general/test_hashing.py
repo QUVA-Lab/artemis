34b859062a1c3f31c0a661b7a1e7a8285670476c
Peter O'Connor <peter.ed.oconnor@gmail.com>
2017-09-09 18:12:48 +0200

    oook

--- artemis/general/test_hashing.py
+++ artemis/general/test_hashing.py
@@ -11,12 +11,12 @@ def test_compute_fixed_hash():
     complex_obj = [1, 'd', {'a': 4, 'b': np.arange(10)}, (7, list(range(10)))]
     original_code = compute_fixed_hash(complex_obj)
 
-    expected_code = 'c9b83dd2e1099c3bcbb05e3c69327c72' if _IS_PYTHON_3 else '285f0ff77c0cb6a5fc529ae55775f9dd'
+    expected_code = 'c9b83dd2e1099c3bcbb05e3c69327c72' if _IS_PYTHON_3 else '6c98fabc301361863f321f6149a8a12a'
 
     assert compute_fixed_hash(complex_obj) == original_code == expected_code
     complex_obj[2]['b'][6]=0
 
-    expected_code = 'a783b1f098fca8ccaf977a3123be5ac4' if _IS_PYTHON_3 else '3124854bda236cd50f0cc04bd6f84935'
+    expected_code = 'a783b1f098fca8ccaf977a3123be5ac4' if _IS_PYTHON_3 else '8aee1f739fc9a612ed72e14682026627'
     assert compute_fixed_hash(complex_obj) == expected_code != original_code
     complex_obj[2]['b'][6]=6  # Revert to old value
     assert compute_fixed_hash(complex_obj) == original_code
