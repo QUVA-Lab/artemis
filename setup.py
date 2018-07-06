586f8ce50909c029def854f4d0a6c40323df00c1
Peter O'Connor <peter.ed.oconnor@gmail.com>
2018-07-06 12:09:18 +0200

    tried reverting version thing

--- setup.py
+++ setup.py
@@ -1,17 +1,5 @@
 from setuptools import setup, find_packages
 
-# Get the version, following advice from https://stackoverflow.com/a/7071358/851699
-import re
-VERSIONFILE="artemis/artemis/_version.py"
-verstrline = open(VERSIONFILE, "rt").read()
-VSRE = r"^__version__ = ['\"]([^'\"]*)['\"]"
-mo = re.search(VSRE, verstrline, re.M)
-if mo:
-    verstr = mo.group(1)
-else:
-    raise RuntimeError("Unable to find version string in %s." % (VERSIONFILE,))
-
-
 setup(
     name='artemis-ml',
     author='Peter & Matthias',
@@ -22,6 +10,6 @@ setup(
     extras_require = {
         'remote_plotting': ["paramiko", "netifaces"]
         },
-    version=verstr,
+    version='2.1.0',
     packages=find_packages(),
     scripts=[])
